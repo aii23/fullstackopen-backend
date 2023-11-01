15e3d9da58316dab9f153713c3ac0f112d389059
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2023-11-01 10:35:27 +0300

    validation added

--- index.js
+++ index.js
@@ -67,8 +67,12 @@ app.put("/api/persons/:id", (request, response, next) => {
                 Persons.findByIdAndUpdate(
                     person.id,
                     { number: newPerson.number },
-                    { new: true }
-                ).then((person) => response.json(person));
+                    { new: true, runValidators: true, context: "query" }
+                )
+                    .then((person) => response.json(person))
+                    .catch((error) => {
+                        next(error);
+                    });
             } else {
                 response.status(404).end();
             }
@@ -111,6 +115,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === "CastError") {
         return response.status(400).send({ error: "malformatted id" });
+    } else if (error.name === "ValidationError") {
+        return response.status(400).json({ error: error.message });
     }
 
     next(error);
